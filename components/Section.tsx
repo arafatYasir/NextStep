1a657559bc00cd19407f9f66ec33757a4c049fd9
iamrafsanrafi <arafat.yasir1305@gmail.com>
2026-01-12 04:42:55 +0600

    Update Section.tsx

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,6 +1,6 @@
 const Section = ({ sectionId, children }: { sectionId: string; children: React.ReactNode }) => {
     return (
-        <section id={sectionId}>{children}</section>
+        <section id={sectionId} className="py-25 space-y-20">{children}</section>
     )
 }
 
