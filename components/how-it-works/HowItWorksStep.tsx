02bc81e2d37ff967545e2ffcb8c13bbf5b8143b6
iamrafsanrafi <arafat.yasir1305@gmail.com>
2026-01-21 07:33:32 +0600

    How it works section full responsive done!

--- components/how-it-works/HowItWorksStep.tsx
+++ components/how-it-works/HowItWorksStep.tsx
@@ -16,7 +16,7 @@ const HowItWorksStep = ({ step }: { step: Props }) => {
 
     return (
         <div className={cn(
-            `flex flex-col-reverse gap-5 md:gap-10 items-center justify-center p-4 border border-transparent hover:border-[rgb(var(--border-hover))] active:border-[rgb(var(--border-hover))] bg-card hover:bg-[rgb(var(--bg-primary))]/5 active:bg-[rgb(var(--bg-primary))]/5 transition-all duration-250 py-4 rounded-xl relative group shadow-lg`,
+            `flex flex-col-reverse gap-5 lg:gap-10 items-center justify-center w-full max-w-[480px] md:max-w-full mx-auto p-4 border border-transparent hover:border-[rgb(var(--border-hover))] active:border-[rgb(var(--border-hover))] bg-card hover:bg-[rgb(var(--bg-primary))]/5 active:bg-[rgb(var(--bg-primary))]/5 transition-all duration-250 py-4 rounded-xl relative group shadow-lg`,
             isEven ? "md:flex-row-reverse" : "md:flex-row",
 
         )}>
@@ -29,21 +29,21 @@ const HowItWorksStep = ({ step }: { step: Props }) => {
 
             {/* ---- Description Part ---- */}
             <div className={cn(
-                "w-full md:w-1/2 space-y-6",
+                "w-full md:w-1/2 space-y-5",
                 isEven ? "md:text-right" : "text-left"
             )}>
-                <h3 className="text-xl xs:text-2xl sm:text-3xl md:text-4xl font-bold tracking-tight text-foreground group-hover:text-[rgb(var(--bg-primary-hover))] group-active:text-[rgb(var(--bg-primary-hover))] transition-all duration-250 font-heading">
+                <h3 className="text-xl xs:text-2xl sm:text-3xl lg:text-4xl font-bold tracking-tight text-foreground group-hover:text-[rgb(var(--bg-primary-hover))] group-active:text-[rgb(var(--bg-primary-hover))] transition-all duration-250 font-heading">
                     {title}
                 </h3>
 
                 {/* ---- Main Description ---- */}
-                <div className="text-sm xs:text-base md:text-lg text-muted-foreground leading-relaxed [&_a]:text-[rgb(var(--bg-primary))] [&_a]:font-medium [&_a]:hover:underline [&_a]:hover:text-[rgb(var(--bg-primary-hover))] group-hover:text-foreground group-active:text-foreground transition-all duration-250 font-sans">
+                <div className="text-sm xs:text-base lg:text-lg text-muted-foreground leading-relaxed [&_a]:text-[rgb(var(--bg-primary))] [&_a]:font-medium [&_a]:hover:underline [&_a]:hover:text-[rgb(var(--bg-primary-hover))] group-hover:text-foreground group-active:text-foreground transition-all duration-250 font-sans">
                     {description}
                 </div>
             </div>
 
             {/* ---- Image Part ---- */}
-            <div className="w-full md:w-[35%] max-w-[400px] hover:scale-105 active:scale-105 transition-all duration-400 aspect-square relative overflow-hidden rounded-xl bg-white/5 shadow-xl">
+            <div className="w-full md:w-[45%] lg:w-[35%] hover:scale-105 active:scale-105 transition-all duration-400 aspect-square relative overflow-hidden rounded-xl bg-white/5 shadow-xl">
                 <Image
                     src={image}
                     width={400}
