839b4e9ab413532bb1715ecca702b79480f25f07
iamrafsanrafi <arafat.yasir1305@gmail.com>
2026-01-21 07:56:25 +0600

    Applied responsive design codes for Insight section

--- components/insights/InsightSection.tsx
+++ components/insights/InsightSection.tsx
@@ -7,14 +7,13 @@ const InsightSection = () => {
     return (
         <Container>
             <Section>
-                <div className="text-center space-y-5 max-w-5xl mx-auto">
-                    <h2 className="text-4xl md:text-5xl font-bold tracking-tight text-foreground leading-[1.2] font-heading">
-                        It's Not That You're Underqualified. <br className="hidden md:block" />
+                <div className="text-center space-y-6 max-w-5xl mx-auto flex flex-col items-center">
+                    <h2 className="text-2xl xs:text-3xl sm:text-4xl md:text-5xl font-bold font-heading text-center tracking-tight text-foreground leading-[1.2]">
+                        It's Not That You're Underqualified. <br />
                         <span className="bg-clip-text text-transparent bg-linear-to-r from-[rgb(var(--bg-primary))]/80 to-[rgb(var(--bg-primary-hover))]">You're Playing The Hiring Game Blind.</span>
                     </h2>
-                    <p className="text-lg font-sans text-muted-foreground text-center leading-relaxed">
+                    <p className="text-sm sm:text-base md:text-lg font-sans text-foreground/80 max-w-2xl leading-relaxed">
                         Most developer resumes fail even before a human ever sees them.
-                        <br className="hidden md:block" />
                         Not because of skills or experience — but because they don't align with how hiring systems actually work.
                     </p>
                 </div>
@@ -22,7 +21,7 @@ const InsightSection = () => {
                 {/* ---- Insight Cards ---- */}
                 <div style={{
                     display: "grid",
-                    gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+                    gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))",
                     gap: "20px"
                 }}>
                     {insights.map((item, index) => (
