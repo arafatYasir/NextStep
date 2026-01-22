ac2422f92b7e06a16b70fa96fb4f0f25dd5de266
iamrafsanrafi <arafat.yasir1305@gmail.com>
2026-01-22 09:55:41 +0600

    Offers section responsive design done

--- components/offers/Offer.tsx
+++ components/offers/Offer.tsx
@@ -2,7 +2,7 @@ const Offer = ({ offer }: { offer: OfferItem }) => {
     const { id, title, description } = offer;
 
     return (
-        <li className="flex items-stretch gap-x-5 group">
+        <li className="flex items-stretch gap-x-3 sm:gap-x-5 group">
             {/* ---- Left Side Indicator w/ Connector ---- */}
             <div className="flex flex-col items-center shrink-0">
                 <div id={`offer-dot-${id}`} className="flex items-center justify-center w-7 h-7 rounded-full bg-[rgb(var(--bg-primary-hover))] font-sans text-white border border-[rgb(var(--border-default))] z-10">
@@ -15,8 +15,8 @@ const Offer = ({ offer }: { offer: OfferItem }) => {
 
             {/* ---- Right Side Content ---- */}
             <div className="pb-5 group-last:pb-0">
-                <h3 className="text-2xl font-heading font-bold text-foreground leading-none py-px">{title}</h3>
-                <p className="text-muted-foreground font-sans leading-normal mt-2">{description}</p>
+                <h3 className="text-lg xs:text-xl sm:text-2xl font-heading font-bold text-foreground leading-none py-px">{title}</h3>
+                <p className="text-sm sm:text-basetext-foreground/80 font-sans leading-normal mt-2">{description}</p>
             </div>
         </li>
     )
--- components/offers/OffersSection.tsx
+++ components/offers/OffersSection.tsx
@@ -8,11 +8,11 @@ const OffersSection = () => {
     return (
         <Container>
             <Section className="py-20 space-y-20">
-                <h2 className="text-center max-w-5xl mx-auto text-4xl md:text-5xl font-heading font-bold tracking-tight text-foreground leading-[1.2]">
+                <h2 className="text-2xl xs:text-3xl sm:text-4xl md:text-5xl font-bold font-heading text-center tracking-tight text-foreground leading-[1.2]">
                     What You Get From NextStep
                 </h2>
 
-                <div className="flex flex-col items-center lg:flex-row lg:items-start gap-x-10 gap-y-10">
+                <div className="flex flex-col-reverse items-center md:flex-row md:items-start gap-x-5 lg:gap-x-10 gap-y-5 lg:gap-y-10 max-w-[480px] md:max-w-full mx-auto">
                     {/* ---- Offers Steps ---- */}
                     <ul className="flex flex-3 flex-col">
                         {offers.map((offer) => (
@@ -21,7 +21,7 @@ const OffersSection = () => {
                     </ul>
 
                     {/* ---- Image ---- */}
-                    <div className="flex-2 max-w-[500px] aspect-square rounded-lg bg-[rgb(var(--bg-primary))] overflow-hidden">
+                    <div className="flex-2 aspect-square rounded-lg bg-[rgb(var(--bg-primary))] overflow-hidden">
                         <Image
                             src="/images/person.webp"
                             width={500}
