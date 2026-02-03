fe4a74f44fe4f7a1793adc7bf3d4c3e9cf63cdf5
iamrafsanrafi <arafat.yasir1305@gmail.com>
2026-02-03 07:06:24 +0600

    Updated analyzeJobDescription function inngest function to include steps

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -6,38 +6,56 @@ import JobRecord from "../models/jobRecord.model";
 
 // Analyze Job Description
 export const analyzeJobDescription = inngest.createFunction(
-    { id: "analyze/job-description" },
+    {
+        id: "analyze/job-description",
+        concurrency: {
+            limit: 5
+        },
+        throttle: {
+            limit: 10,
+            period: "1m"
+        }
+    },
     { event: "analyze/job-description" },
     async ({ event, step }) => {
         const { jobId, jobRole, jobDescription } = event.data;
 
         try {
-            // Creating AI instance
+            // Create AI instance
             const genAI = new GoogleGenerativeAI(process.env.JOB_ANALYSIS_AI_API_KEY!);
 
-            // Choosing model
+            // Choose a model
             const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
 
-            // Generating dynamic prompt
-            const prompt = JOB_ANALYSIS_PROMPT.replace("{jobRole}", jobRole).replace("{jobDescription}", jobDescription);
+            //  Generate AI analysis
+            const parsedJSONData = await step.run("generate-ai-analysis", async () => {
+                // Generate dynamic prompt
+                const prompt = JOB_ANALYSIS_PROMPT.replace("{jobRole}", jobRole).replace("{jobDescription}", jobDescription);
 
-            // Generating response
-            const result = await model.generateContent(prompt);
+                // Generate response
+                const result = await model.generateContent(prompt);
 
-            // Cleaning response and parsing to json
-            const response = cleanAIResponse(result.response.text());
-            const parsedJSONData = JSON.parse(response);
+                // Clean response and parse to json
+                const response = cleanAIResponse(result.response.text());
+                const parsedJSONData = JSON.parse(response);
 
-            // Updating job record
-            await JobRecord.findByIdAndUpdate(jobId, {
-                status: "completed",
-                result: parsedJSONData
+                return parsedJSONData;
             });
+
+            // Update job record
+            await step.run("update-job-recod", async () => {
+                await JobRecord.findByIdAndUpdate(jobId, {
+                    status: "completed",
+                    result: parsedJSONData
+                });
+            })
         } catch (e: any) {
             console.error("Failed to analyze job (Inngest Function)", e);
 
-            await JobRecord.findByIdAndUpdate(jobId, {
-                status: "failed"
+            await step.run("update-job-recod", async () => {
+                await JobRecord.findByIdAndUpdate(jobId, {
+                    status: "failed"
+                });
             });
         }
     }
