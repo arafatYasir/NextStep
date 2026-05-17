e1cf9f17bd90d37229ee383dde47ff27fdf96d15
arafatYasir <arafat.yasir1305@gmail.com>
2026-05-17 13:42:51 +0600

    Removed functions of ingnest for testing

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,12 +2,9 @@
 
 import { serve } from "inngest/next";
 import { inngest } from "@/src/inngest/client";
-import { analyzeJobDescription, analyzeResume } from "@/src/inngest/functions";
+// import { analyzeJobDescription, analyzeResume } from "@/src/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    functions: [
-        analyzeJobDescription,
-        analyzeResume
-    ],
+    functions: [],
 });
