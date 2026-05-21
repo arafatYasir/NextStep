d47cb9cc53315d4440a204ae15bf04101fca5da0
arafatYasir <arafat.yasir1305@gmail.com>
2026-05-21 18:24:45 +0600

    Added a tiny loading skeleton for 'file type' inside a resume card.

--- components/dashboard/resume-analyses/SingleResumeCardSkeleton.tsx
+++ components/dashboard/resume-analyses/SingleResumeCardSkeleton.tsx
@@ -1,12 +1,15 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
 export const SingleResumeCardSkeleton = () => (
-    <div className="flex items-start gap-4  rounded-xl border border-[rgb(var(--border-default))] bg-card p-4">
+    <div className="flex items-start gap-4 relative rounded-xl border border-[rgb(var(--border-default))] bg-card p-4">
         <Skeleton className="size-11 shrink-0" />
 
         <div className="space-y-3 w-1/2">
             <Skeleton className="h-4 rounded w-full" />
             <Skeleton className="h-3 rounded w-1/2" />
+
+            {/* File type skeleton */}
+            <Skeleton className="h-4 w-9 rounded-full absolute top-4 right-30" />
 
             <div className="flex gap-4">
                 <Skeleton className="h-3 rounded w-1/5" />
