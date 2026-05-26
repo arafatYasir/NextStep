04a345c623ebbd680068fbd170c79e7b52b820fc
arafatYasir <arafat.yasir1305@gmail.com>
2026-05-26 15:33:51 +0600

    Created the resume schema

---
+++ src/models/resume.model.ts
@@ -0,0 +1,113 @@
+import mongoose from "mongoose";
+
+const Resume = new mongoose.Schema({
+    personalInfo: {
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        location: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        github: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        linkedin: {
+            type: String,
+            required: true,
+            trim: true,
+        }
+    },
+    summary: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    skills: {
+        languages: [String],
+        frameworksAndLibraries: [String],
+        toolsAndPlatforms: [String],
+        softSkills: [String]
+    },
+    experience: [
+        {
+            jobTitle: {
+                type: String,
+                required: true,
+                trim: true,
+            },
+            company: {
+                type: String,
+                required: true,
+                trim: true,
+            },
+            duration: {
+                type: String,
+                required: true,
+                trim: true,
+            },
+            responsibilities: {
+                type: [String],
+                trim: true,
+            }
+        }
+    ],
+    projects: [
+        {
+            projectName: {
+                type: String,
+                required: true,
+                trim: true,
+            },
+            techStack: {
+                type: [String],
+                required: true,
+            },
+            highlights: {
+                type: [String],
+                required: true,
+            },
+            link: {
+                type: String,
+                required: true,
+                trim: true,
+            }
+        }
+    ],
+    education: [
+        {
+            degree: {
+                type: String,
+                required: true,
+                trim: true,
+            },
+            institution: {
+                type: String,
+                required: true,
+                trim: true,
+            },
+            duration: {
+                type: String,
+                required: true,
+                trim: true,
+            }
+        }
+    ]
+}, { timestamps: true });
+
+export default mongoose.models.Resume || mongoose.model("Resume", Resume);
