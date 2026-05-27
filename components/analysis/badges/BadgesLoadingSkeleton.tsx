a0184c49bdaf707a2d83edc777e21ce54ec72afd
arafatYasir <arafat.yasir1305@gmail.com>
2026-05-27 21:48:58 +0600

    Updated the design of job description analysis modal loading skeleton

--- components/analysis/badges/BadgesLoadingSkeleton.tsx
+++ components/analysis/badges/BadgesLoadingSkeleton.tsx
@@ -1,12 +1,17 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
-const BadgesLoadingSkeleton = ({count} : {count: number}) => {
+const BadgesLoadingSkeleton = ({ count }: { count: number }) => {
     const badges = Array.from({ length: count }).fill(0);
 
     return (
         <div className="flex flex-wrap gap-3">
             {badges.map((_, index) => (
-                <Skeleton key={index} className="w-[120px] h-[34px]" />
+                <div className="bg-gray-100 rounded-lg border border-[rgb(var(--border-default))]" key={index}>
+                    <div className="pl-3 pr-2 py-[7px] flex items-center gap-2">
+                        <Skeleton className="w-20 h-5 rounded" />
+                        <Skeleton className="size-5 rounded-md" />
+                    </div>
+                </div>
             ))}
         </div>
     )
