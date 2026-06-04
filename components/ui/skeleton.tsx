fbdb9cbd17d2bdde64e6ff39176dab4b8c4f4935
arafatYasir <arafat.yasir1305@gmail.com>
2026-06-04 18:44:07 +0600

    Updated the design of skeleton

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -4,7 +4,7 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-[rgb(var(--border-default))] animate-pulse rounded-lg", className)}
+      className={cn("bg-[rgb(var(--border-default))] relative overflow-hidden before:content-[''] before:absolute before:inset-0 before:bg-linear-to-r before:from-transparent before:via-white/50 before:to-transparent before:animate-shimmer rounded-lg", className)}
       {...props}
     />
   )
